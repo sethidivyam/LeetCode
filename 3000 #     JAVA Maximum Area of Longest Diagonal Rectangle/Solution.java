089f2db21e55460849850a427da1f79abefa7bc7
sethidivyam <divyamsethi1804@gmail.com>
2025-09-09 00:00:39 +0530

    update

--- 3000 #     JAVA Maximum Area of Longest Diagonal Rectangle/Solution.java
+++ 3000 #     JAVA Maximum Area of Longest Diagonal Rectangle/Solution.java
@@ -1,11 +1,6 @@
 /*
 3000. Maximum Area of Longest Diagonal Rectangle
-Solved
-Easy
-Topics
-premium lock icon
-Companies
-Hint
+
 You are given a 2D 0-indexed integer array dimensions.
 
 For all indices i, 0 <= i < dimensions.length, dimensions[i][0] represents the length and dimensions[i][1] represents the width of the rectangle i.
